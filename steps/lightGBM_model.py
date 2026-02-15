6de9d57138e75bf549831ae40b4477b3cf991d71
chris026 <73955518+chris026@users.noreply.github.com>
2026-02-16 00:26:58 +0100

    popularity frames fixed

--- steps/lightGBM_model.py
+++ steps/lightGBM_model.py
@@ -1,7 +1,7 @@
 from zenml import step
 import polars as pl
 
-@step
+@step(enable_cache=False)
 def prepare_data(W2Vmodel_path: str, data_path, commerces_path, products_path):
     orders = pl.scan_parquet(data_path)
     commerces = pl.scan_parquet(commerces_path)
@@ -10,10 +10,12 @@ def prepare_data(W2Vmodel_path: str, data_path, commerces_path, products_path):
     products = pl.scan_parquet(products_path)
 
     # global popularity
-    pop_global = orders.group_by("productid")
-    pop_store = orders.group_by(["userid","productid"])
-    pop_region = orders.group_by(["region","productid"])
-    pop_subch = orders.group_by(["subchannel","productid"])
-    pop_origin = orders.group_by(["origin","productid"])
+    pop_global = orders.group_by("productid").agg(pl.count().alias("pop_global"))
+    pop_store = orders.group_by(["userid","productid"]).agg(pl.count().alias("pop_store"))
+    pop_region = orders.group_by(["region","productid"]).agg(pl.count().alias("pop_region"))
+    pop_subch = orders.group_by(["subchannel","productid"]).agg(pl.count().alias("pop_subch"))
+    pop_origin = orders.group_by(["origin","productid"]).agg(pl.count().alias("pop_origin"))
 
     print(pop_global.head(5).collect())
+    print(pop_global.filter(pl.col("productid") == "000051-007").collect())
+    
